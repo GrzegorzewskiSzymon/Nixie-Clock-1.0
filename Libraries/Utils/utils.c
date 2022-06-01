87985d0e62b6d612dbbae580a5946d6a738b86ef
Szymon Grzegorzewski <szymon.s.grzegorzewski@gmail.com>
2022-06-01 18:10:36 +0200

    add non blocking functions to count number of presses on buttons, along with fast counting after pressing button for over 2,5sec (can be easily change in utils.h) this implemantation require timer 0 in CTC mode to increase variable in exactly each 1ms

--- Libraries/Utils/utils.c
+++ Libraries/Utils/utils.c
@@ -5,10 +5,9 @@
  *      Author: Grzegorzewski Szymon
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "utils.h"
-#include <avr/interrupt.h>
-
 void Led_Init()
 {
 	LED0_INIT;
@@ -38,3 +37,157 @@ ISR(TIMER0_COMPA_vect)
 {
 	millis++;
 }
+
+SWITCH Switch_0;
+SWITCH Switch_1;
+void SwitchInit(SWITCH *Switch_0, SWITCH *Switch_1)
+{
+	Switch_0->switchNumber = 0;
+	Switch_1->switchNumber = 1;
+}
+
+
+static void Idle(SWITCH *switch_)
+{
+
+	if(switch_->switchNumber == 0)
+	{
+		if(SW_0_IS_PUSHED)//possibility of being pressed
+		{
+			switch_->timerDebounce = millis;
+			switch_->switchState = DEBOUNCE;
+		}
+	}
+	else
+	{
+		if(SW_1_IS_PUSHED)//possibility of being pressed
+		{
+			switch_->timerDebounce = millis;
+			switch_->switchState = DEBOUNCE;
+		}
+	}
+}
+
+static void Debounce(SWITCH *switch_)
+{
+	if(millis-switch_->timerDebounce > DEBOUNCE_TIME)
+	{
+		if(switch_->switchNumber == 0)
+		{
+			if(SW_0_IS_PUSHED)//if pressed
+			{
+				switch_->counterData++;
+				switch_->timerPress = millis;
+				switch_->switchState = PRESSED;
+			}
+		}
+		else
+		{
+			if(SW_1_IS_PUSHED)//if pressed
+			{
+				switch_->counterData++;
+				switch_->timerPress = millis;
+				switch_->switchState = PRESSED;
+			}
+		}
+	}
+}
+
+
+static void Pressed(SWITCH *switch_)
+{
+	if(switch_->switchNumber == 0)
+	{
+		if(SW_0_IS_PUSHED)//if still pressed
+		{
+			if(millis-switch_->timerPress > LONG_PRESS_TIME)
+			{
+				switch_->timerLongPress = millis;
+				switch_->switchState = LONG_PRESS;
+			}
+		}
+		else
+		{
+			switch_->switchState = RELEASED;
+		}
+	}
+	else
+	{
+		if(SW_1_IS_PUSHED)//if still pressed
+		{
+			if(millis-switch_->timerPress > LONG_PRESS_TIME)
+			{
+				switch_->timerLongPress = millis;
+				switch_->switchState = LONG_PRESS;
+			}
+		}
+		else
+		{
+			switch_->switchState = RELEASED;
+		}
+	}
+
+}
+
+
+static void LongPress(SWITCH *switch_)
+{
+	if(millis-switch_->timerLongPress > LONG_PRESS_TIME_CYCLE)
+	{
+		if(switch_->switchNumber == 0)
+		{
+			if(SW_0_IS_PUSHED)//if pressed
+			{
+				switch_->counterData++;
+				switch_->timerLongPress = millis;
+			}
+			else
+			{
+				switch_->switchState = RELEASED;
+			}
+		}
+		else
+		{
+			if(SW_1_IS_PUSHED)//if pressed
+			{
+				switch_->counterData++;
+				switch_->timerLongPress = millis;
+			}
+			else
+			{
+				switch_->switchState = RELEASED;
+			}
+		}
+
+	}
+}
+
+
+void StateMachine(SWITCH *switch_)
+{
+	switch(switch_->switchState)
+	{
+		case IDLE:
+			Idle(switch_);
+			break;
+
+		case DEBOUNCE:
+			Debounce(switch_);
+			break;
+
+		case PRESSED:
+			Pressed(switch_);
+			break;
+
+		case LONG_PRESS:
+			LongPress(switch_);
+			break;
+
+		case RELEASED:
+			switch_->switchState = IDLE;
+			break;
+	}
+}
+
+
+
--- Libraries/Utils/utils.h
+++ Libraries/Utils/utils.h
@@ -8,6 +8,9 @@
 #ifndef LIBRARIES_UTILS_UTILS_H_
 #define LIBRARIES_UTILS_UTILS_H_
 
+/////////////////////////////////////////////////////////
+//                   LEDs
+/////////////////////////////////////////////////////////
 #define LED0_INIT DDRB |=  (1<<PB1)
 #define LED0_ON  PORTB |=  (1<<PB1)
 #define LED0_OFF PORTB &=~ (1<<PB1)
@@ -20,9 +23,55 @@
 
 void Led_Init();
 void BlinkLed();
+/////////////////////////////////////////////////////////
 
 
-void Timer0_Init();//millis
+/////////////////////////////////////////////////////////
+//                   SWITCHES
+/////////////////////////////////////////////////////////
+
+#define SW_0_IS_PUSHED !(PIND & (1<<PD5))
+#define SW_1_IS_PUSHED !(PIND & (1<<PD6))
+
+#define DEBOUNCE_TIME 20 //ms;
+#define LONG_PRESS_TIME 2500 //ms;  time to press turns to long press
+#define LONG_PRESS_TIME_CYCLE 150 //ms;
+
+typedef enum
+{
+	IDLE = 0,
+	DEBOUNCE,
+	PRESSED,
+	LONG_PRESS,
+	RELEASED
+
+}SWITCH_STATE;
+
+typedef struct
+{
+	SWITCH_STATE switchState;
+	uint8_t switchNumber;
+
+	uint16_t counterData;
+
+	uint32_t timerDebounce;
+	uint32_t timerPress;
+	uint32_t timerLongPress;
+
+}SWITCH;
+
+extern SWITCH Switch_0;
+extern SWITCH Switch_1;
+void SwitchInit(SWITCH *switch_0, SWITCH *switch_1);
+void StateMachine(SWITCH *switch_);
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//                   TIMERS
+/////////////////////////////////////////////////////////
+void Timer0_Init();//start counting millis
 extern uint64_t millis;
+/////////////////////////////////////////////////////////
 
 #endif /* LIBRARIES_UTILS_UTILS_H_ */
